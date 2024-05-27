b5bf57ae7e87bc31ae31116d0f063bb52d3d4de7
ah5087 <alicehou3302@gmail.com>
2024-05-27 00:32:17 -0400

    added email icon

--- src/pages/IntroSection/introsection.tsx
+++ src/pages/IntroSection/introsection.tsx
@@ -1,15 +1,12 @@
+import React from "react";
 import styles from "./Intro.module.scss";
-import commonStyles from "../../styles/Common.module.scss";
 
 const IntroSection = () => (
-  <section
-    id="intro"
-    className={`${commonStyles.section} ${styles.introSection}`}
-  >
+  <section id="intro" className={styles.introSection}>
     <div className={styles.paragraphContainer}>
-      <h3>hey there! 👋</h3>
+      <h3>hey there!</h3>
       <p className={styles.paragraph}>
-        I'm Alice, a junior studying Computer Science at Princeton. My first
+        I'm Alice, a junior studying Computer Science at Princeton! My first
         time coding was as a freshman in university, and I've since organized 4
         hackathons and learned 7 programming languages! My interests lie in
         machine learning, distributed systems, and full-stack development. I
@@ -17,13 +14,15 @@ const IntroSection = () => (
         grow, so please feel free to reach out! I'd love to chat.
       </p>
       <p className={styles.paragraph}>
+        <img src="/email.svg" alt="Email" className={styles.emailIcon} />
         <span className={styles.typewriter}>
-          <b>ah5087 [at] princeton [dot] edu</b>
+          ah5087 [at] princeton [dot] edu
         </span>
       </p>
     </div>
     <div className={styles.photoContainer}>
       <img src="/headshot.jpg" className={styles.photo} alt="Alice Hou" />
+      <div className={styles.introTitle}>Alice Hou</div>
     </div>
   </section>
 );
